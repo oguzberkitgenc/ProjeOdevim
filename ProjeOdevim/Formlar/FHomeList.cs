39d47aec8d65cb54a38ff38673eda9d6c5d6e3c7
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2022-04-03 21:43:44 +0300

    FHomeList 3 Grid Finish

--- ProjeOdevim/Formlar/FHomeList.cs
+++ ProjeOdevim/Formlar/FHomeList.cs
@@ -21,17 +21,47 @@ namespace ProjeOdevim.Formlar
         void DecliningStok()
         {
             connection.Open();
-            SqlDataAdapter adapter = new SqlDataAdapter("Select TOP 10 TBLURUN.ID,MARKAADI AS 'MARKA',URUNADI AS 'ÜRÜN ADI'," +
+            SqlDataAdapter adapter = new SqlDataAdapter("Select TOP 10 TBLURUN.ID as 'No',MARKAADI AS 'MARKA',URUNADI AS 'ÜRÜN ADI'," +
                 "SATISFIYAT AS 'SATIŞ FİYATI',STOK,ACIKLAMA AS 'AÇIKLAMA' From TBLURUN INNER JOIN TBLMARKA ON " +
-                "TBLURUN.MARKAID=TBLMARKA.ID ORDER BY STOK ASC ",connection);
-            connection.Close();
+                "TBLURUN.MARKAID=TBLMARKA.ID ORDER BY STOK ASC ", connection);
             DataTable dataTable = new DataTable();
             adapter.Fill(dataTable);
             gridControl1.DataSource = dataTable;
+            connection.Close();
+        }
+        void NewEmployee()
+        {
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("Select TOP 10 TBLPERSONEL.ID,DEPARTMAN,MAGAZA,AD +' '+ SOYAD AS 'AD SOYAD'" +
+                "From TBLPERSONEL  INNER JOIN TBLDEPARTMAN ON TBLPERSONEL.DEPARTMANID=TBLDEPARTMAN.ID " +
+                "INNER JOIN TBLMAGAZA ON TBLPERSONEL.MAGAZAID=TBLMAGAZA.ID " +
+                "WHERE TBLPERSONEL.DEPARTMANID!=(SELECT ID FROM TBLDEPARTMAN WHERE DEPARTMAN='Stajer') " +
+                "ORDER BY TBLPERSONEL.ID DESC", connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            gridControl3.DataSource = dataTable;
+            connection.Close();
+        }
+        void NewStajer()
+        {
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("Select TOP 10 TBLPERSONEL.ID,DEPARTMAN,MAGAZA,AD +' '+ SOYAD AS 'AD SOYAD' " +
+                "From TBLPERSONEL  INNER JOIN TBLDEPARTMAN ON TBLPERSONEL.DEPARTMANID=TBLDEPARTMAN.ID " +
+                "INNER JOIN TBLMAGAZA ON TBLPERSONEL.MAGAZAID=TBLMAGAZA.ID " +
+                "WHERE TBLPERSONEL.DEPARTMANID=(SELECT ID FROM TBLDEPARTMAN WHERE DEPARTMAN='Stajer') " +
+                "ORDER BY TBLPERSONEL.ID DESC", connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            gridControl5.DataSource = dataTable;
+            connection.Close();
         }
         private void FHomeList_Load(object sender, EventArgs e)
         {
             DecliningStok();
+            NewEmployee();
+            NewStajer();
+            gridView3.Columns[0].Visible = false;
+            gridView5.Columns[0].Visible = false;
         }
     }
 }
